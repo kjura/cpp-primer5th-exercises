0a2411a504d6e75325c0d67d524a80c3e83a357d
kjura <kjura@users.noreply.github.com>
2022-01-24 19:39:16 +0100

    Upload unfinished exercise27.
    This is still not finished. TODO later.

---
+++ chapter2/exercise027/exercise27.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+// g++ -std=c++11 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp -o sandbox
+// g++ -std=c++11 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// Section 2.4.2, exercise 2.27  <br />
+
+
+int main() 
+{   
+/*
+    int i = -1, &r = 0; // ILLEGAL -> nonconst reference must be bound to an already existing object
+
+    int *const p2 = &i2; // LEGAL if i2 was previously defined
+
+    const int i = -1, &r = 0; // LEGAL
+
+    const int *const p3 = &i2; // LEGAL if i2 previously defined as const int
+
+    const int *p1 = &i2; // LEGAL if i2 previously defined as 
+
+    const int &const r2; // ILLEGAL const reference must be initalized
+
+    const int i2 = i, &r = i; //
+*/
+    
+    return 0;
+
+}
