7005189d2822bbc402e53c676bb4554eb5fc8c1b
kjura <kjura@users.noreply.github.com>
2022-01-26 14:31:54 +0100

    Upload exercise 29 - not finished
    TODO later.

---
+++ chapter2/exercise029/exercise29.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+// g++ -std=c++11 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp -o sandbox
+// g++ -std=c++11 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+
+
+int main() 
+{   
+    
+    //int i, *const cp; // (a)
+    //int *p1, *const p2; // (b)
+    //const int ic, &r = ic; // (c)
+    //const int *const p3; // (d)
+    //const int *p; //(e) 
+
+
+    //i = ic; // LEGAL, converting nonconst int to const int
+    
+    
+
+    
+
+
+
+    return 0;
+
+}
