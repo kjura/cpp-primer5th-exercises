fdbd28e86eaf0a3b0f3eb3587c297bc27da517d6
kjura <bulsit565@gmail.com>
2022-01-30 20:59:33 +0100

    Fighting with git about ignoring sandbox.cpp  :/

---
+++ sandbox.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// g++ -std=c++11 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp -o sandbox
+// g++ -std=c++11 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+
+
+int main() 
+{   
+    
+    
+
+    return 0;
+
+}
