26f2c4c3928892707f9ace222036019dbe438935
kjura <bulsit565@gmail.com>
2022-02-04 18:06:55 +0100

    Add exercise 20 and helper input/output files

--- chapter1/exercise020/exercise20.cpp
+++ chapter1/exercise020/exercise20.cpp
@@ -3,10 +3,16 @@
 // g++ -std=c++11 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
 // g++ -std=c++11 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
 
+// To use file redirection: After compilation just write in your terminal (Tested on Linux Ubuntu Distro)
+// ./exercise20 <infile >outfile
 
 int main (){
 
-    
+    Sales_item book; // book contains ISB, number of copies sold and sales price
+
+    while (std::cin >> book){
+        std::cout << book << "\n";
+    }
     
 
     return 0;
