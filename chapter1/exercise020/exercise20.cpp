0432e452f1acdefe1aee076c3d337a2d26b26503
kjura <bulsit565@gmail.com>
2022-02-04 17:17:05 +0100

    Set environment for doing exercise 20

---
+++ chapter1/exercise020/exercise20.cpp
@@ -0,0 +1,13 @@
+#include "Sales_item.h"
+#include <iostream>
+// g++ -std=c++11 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++11 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+
+
+int main (){
+
+    
+    
+
+    return 0;
+}
