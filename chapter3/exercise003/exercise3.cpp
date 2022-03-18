fadecac0513035388053db8878e4f9d0efd5e1f4
kjura <bulsit565@gmail.com>
2022-03-18 15:32:16 +0100

    Add exercise-3.3

---
+++ chapter3/exercise003/exercise3.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <string>
+#include <iostream>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g exercise3.cpp  -o exercise3
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g exercise3.cpp -o exercise3
+// <br />
+
+//using std::string;
+//using std::cout;
+//using std::cin;
+//using std::endl;
+
+int main()
+{
+    
+    /* dasdasdasd 
+    
+    string input operator -> The string input operator reads and discards any leading whitespace.
+    Then it reads the characters until a whitespace is encountered. 
+    It effectively reads the characters but without any whitespace.
+
+
+    getline function -> This function reads the given stream up to and including  the first newline and stores
+    what it read - NOT INCLUDING the newline - in its string argument.
+    After getline sees a newline - it stops reading and returns.
+    
+    */
+
+
+    return 0;
+}
