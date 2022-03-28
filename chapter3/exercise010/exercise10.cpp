fad8c1e0a4ad885ee581294769da61dbc22ff688
kjura <bulsit565@gmail.com>
2022-03-28 10:18:04 +0200

    Create files for exercise 10 chapter 3

---
+++ chapter3/exercise010/exercise10.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <string>
+#include <iostream>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g PLACEHOLDER.cpp  -o PLACEHOLDER
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g PLACEHOLDER.cpp -o PLACEHOLDER
+// <br />
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    
+    
+
+    return 0;
+}
