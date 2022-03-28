c8051cdb520430fa16015462235cc0a62df36073
kjura <bulsit565@gmail.com>
2022-03-28 10:07:02 +0200

    Add finished exercise 11

---
+++ chapter3/exercise011/README.md
@@ -0,0 +1,7 @@
+Exercise 3.11: Is the following range for legal? If so, what is the type of
+c?
+
+```cpp
+const string s = "Keep out!";
+for (auto &c : s) { /* ...  */ }
+```
---
+++ chapter3/exercise011/exercise11.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <string>
+#include <iostream>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g PLACEHOLDER.cpp  -o PLACEHOLDER
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g PLACEHOLDER.cpp -o PLACEHOLDER
+// <br />
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    /*
+    const string s = "Keep out!";
+    for (auto &c : s) { something  } 
+    
+    */
+
+   
+    // CODE IS LEGAL. Type of c: const char &, a reference to const char
+
+    
+
+    return 0;
+}
