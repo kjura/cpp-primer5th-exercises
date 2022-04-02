201d339f0e8e8cb163e663bb818f925c6293206d
kjura <bulsit565@gmail.com>
2022-04-02 18:39:28 +0200

    Create files for exercise 19

---
+++ chapter3/exercise019/README.md
@@ -0,0 +1,3 @@
+Exercise 3.19: List three ways to define a vector and give it ten elements,
+each with the value 42. Indicate whether there is a preferred way to do so
+and why.
---
+++ chapter3/exercise019/exercise19.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <cctype>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g PLACEHOLDER.cpp  -o PLACEHOLDER
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g PLACEHOLDER.cpp -o PLACEHOLDER
+// <br />
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
+
+int main()
+{
+
+    vector<int> v1;
+    vector<int> v2;
+    vector<int> v3;
+
+    
+
+
+    return 0;
+}
