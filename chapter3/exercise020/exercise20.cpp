e8e77c27f5283883ff6ccf1c252069264cc44f5e
kjura <bulsit565@gmail.com>
2022-04-27 22:00:41 +0200

    Add logic for summing adjacent elements

--- chapter3/exercise020/exercise20.cpp
+++ chapter3/exercise020/exercise20.cpp
@@ -18,7 +18,25 @@ using std::vector;
 int main()
 {
 
-    
+    int input_int;
+    std::vector<int> container;
+
+    std::cout << "Please enter your integers to be held by container vector<int>. Press Ctrl + D to stop the input: ";
+    while (cin >> input_int){
+        container.push_back(input_int);
+    }
+
+    // Sum each pair of adjacent elements
+
+    int sum_adj = 0;
+    for (decltype(container.size()) indx = 0; indx != (container.size() - 1); ++indx){
+
+        //cout << container[indx] << "+" << container[indx + 1] << "= " << container[indx] + container[indx + 1] << "\n";
+        sum_adj += container[indx] + container[indx + 1];
+
+    }
+
+    cout << "\nSum of adjacent elements is: " << sum_adj << "\n";
 
     return 0;
 }
