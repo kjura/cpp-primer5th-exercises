46446a77236f1c974b2c8e141d5ed0d0f80deebb
kjura <bulsit565@gmail.com>
2022-07-30 18:09:12 +0200

    add skeleton of exercise 25 along with README.md

---
+++ chapter3/exercise025/README.md
@@ -0,0 +1,2 @@
+Exercise 3.25: Rewrite the grade clustering program from § 3.3.3 (p. 104)
+using iterators instead of subscripts.
---
+++ chapter3/exercise025/exercise25.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <cctype>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g PLACEHOLDER.cpp  -o PLACEHOLDER
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g PLACEHOLDER.cpp -o PLACEHOLDER
+// <br />
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
+
+int main(){
+
+
+    /* Change the below so it works with iterators
+    vector<unsigned> scores(11, 0);
+    unsigned grade;
+    while (cin >> grade){
+
+        if (grade <= 100){
+            ++scores[grade/10];
+        }
+        
+    }
+    
+    for (auto number : scores){
+        cout << number << " ";
+    }
+    cout << "\n"; */
+
+    
+    
+    
+    return 0;
+}
