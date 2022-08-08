f29fd3d0b8906764fd212dfb91f88214e03f4869
kjura <bulsit565@gmail.com>
2022-08-09 00:26:39 +0200

    add finished exercise 28

--- chapter3/exercise028/exercise28.cpp
+++ chapter3/exercise028/exercise28.cpp
@@ -15,12 +15,37 @@ using std::vector;
 // g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g PLACEHOLDER.cpp -o PLACEHOLDER
 // <br />
 
-int main(){
+string sa[10]; // Default initalization --> empty string 
+int ia[10]; // Default initalization --> 10 zeroes
 
-    
+int main() { 
 
-    
+    //string sa2[10]; // Undefined value, defined inside a function
 
+    //int ia2[10]; // Undefined value, defined inside a function
+
+
+    for (auto e : sa){ // Empty string
+        cout << e << " ";
+    }
+    cout << "\n";
+
+
+    for (auto e : ia){ // Zero
+        cout << e << " ";
+    }
+    cout << "\n";
+
+    /*for (auto e : sa2){ // UB fun :)))
+        cout << e << " ";
+    }
+    cout << "\n";
+
+    for (auto e : ia2){ // UB fun :)))
+        cout << e << " ";
+    }
+    cout << "\n"; */
 
     return 0;
-}
+
+    }
