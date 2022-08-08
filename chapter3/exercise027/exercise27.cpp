6428107ac3ac8d3065fd42ab83ae72e03199006c
kjura <bulsit565@gmail.com>
2022-08-09 00:04:24 +0200

    add finished ex 3.27 and build c++11 settings to .vscode json

--- chapter3/exercise027/exercise27.cpp
+++ chapter3/exercise027/exercise27.cpp
@@ -17,6 +17,46 @@ using std::vector;
 
 int main(){
 
+    
+
+    /*
+
+    // (a) Illegal (compilation error with pedantic), not a constant expression, 
+    // error: ISO C++ forbids variable length array ‘ia’
+
+    unsigned buf_size = 1024;
+    int ia[buf_size];
+
+    */ 
+
+    /*
+
+    // (b) Legal
+
+    int ia[4 * 7 - 14];
+
+    */ 
+
+
+    /*
+
+    // (c) 
+    // Illegal, txt_size() is not constexpr (if it is, then it's ok)
+    int ia[txt_size()];
+
+    */ 
+    
+
+    /*
+
+    // (d) 
+    // Illegal, there is no space for a null character, should be st[12]
+    
+    char st[11]= "fundamental"
+
+    */ 
+
+    cout << "I love c++!" << endl;
 
     return 0;
 }
