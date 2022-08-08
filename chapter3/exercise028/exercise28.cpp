c683ed589b35c6687e9c92193552bd3dbd8f2ad4
kjura <bulsit565@gmail.com>
2022-08-09 00:12:42 +0200

    init exercise 28

---
+++ chapter3/exercise028/exercise28.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <cctype>
+#include <limits>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g PLACEHOLDER.cpp  -o PLACEHOLDER
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g PLACEHOLDER.cpp -o PLACEHOLDER
+// <br />
+
+int main(){
+
+    
+
+    
+
+
+    return 0;
+}
