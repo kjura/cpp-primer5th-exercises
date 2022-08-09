2e7c0118a151ef45c4a19c73278981ec6fc675e4
kjura <bulsit565@gmail.com>
2022-08-09 19:09:06 +0200

    automation: add dir and file creation

---
+++ build/automation.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import sys
+
+
+MAINDIR = Path.cwd()
+ROOTDIRS = [x for x in Path('.').iterdir() if x.is_dir()]
+
+
+def createExerciseDirectory(chapterDirName: str, exDirName: str, ):
+
+    pathToDirExercise = MAINDIR / chapterDirName / exDirName
+
+    if not pathToDirExercise.exists():
+        pathToDirExercise.mkdir()
+        return pathToDirExercise
+    else:
+        sys.exit("Directory already exists. Terminating ...")
+
+def createExerciseFiles(pathToDirExercise: Path, exFileName: str):
+
+    pathToCreateExFile = pathToDirExercise / exFileName
+    pathToCreateReadme = pathToDirExercise / 'README.md'
+
+    if not pathToCreateExFile.exists():
+        pathToCreateExFile.touch()
+    else:
+        sys.exit("File already exists. Terminating ...")
+
+    
+    if not pathToCreateReadme.exists():
+        pathToCreateReadme.touch()
+    else:
+        sys.exit("File already exists. Terminating ...")
+
+    
+    
+
+createExerciseDir = createExerciseDirectory('chapter1', 'exercise.1.44')
+createExerciseFiles(createExerciseDir, 'exercise.1.44.cpp')
