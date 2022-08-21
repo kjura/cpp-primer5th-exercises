06ffabaa2f30956a8d79cda36b2f07945f0da561
kjura <bulsit565@gmail.com>
2022-08-21 17:47:05 +0200

    exercise: finish 3.34

--- chapter3/exercise034/exercise34.cpp
+++ chapter3/exercise034/exercise34.cpp
@@ -11,8 +11,17 @@ using std::endl;
 int main()
 {
 
+    int arr[] = {0, 1, 2, 3, 4};
 
+    int *p1 = arr;
+    int *p2 = &arr[3];
 
+    // The following code moves p1 to p2
+    p1 += p2 - p1; 
+    /* This code is illegal if (p2 - p1) is a value such that p1 + this value goes out of the scope of the array
+    then, if we dereferenced this pointer we would have undefined behaviour */
+
+    cout << *p1 << "\n";
 
     return 0;
 }
