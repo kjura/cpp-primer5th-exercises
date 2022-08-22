08893c519024076771a695b3651223f99fcaa97d
kjura <bulsit565@gmail.com>
2022-08-22 20:26:02 +0200

    exercise: add logic for checking length of C-style array

--- chapter3/exercise036/exercise36.cpp
+++ chapter3/exercise036/exercise36.cpp
@@ -7,11 +7,48 @@
 // <br />
 using std::cout;
 using std::endl;
+using std::vector;
+using std::begin;
+using std::end;
 
 int main()
 {
+    // First, element-wise comparison of arr1 and arr2
+    // Two things then, check if length is the same, and then check equality element-wise
 
-    
+    int arrOne[] = {1, 2, 3, 4, 5};
+    int arrTwo[] = {5, 6, 7, 8, 9};
+
+    //int *begArrOne = begin(arrOne);
+    //int *begArrTwo = begin(arrTwo);
+
+    unsigned lengthArrOneCounter = 0;
+    unsigned lengthArrTwoCounter = 0;
+
+    // First calculate length
+
+    for (auto pbeg = begin(arrOne); pbeg != end(arrOne); ++pbeg){
+        ++lengthArrOneCounter;
+    }
+
+    for (auto pbeg = begin(arrTwo); pbeg != end(arrTwo); ++pbeg){
+        ++lengthArrTwoCounter;
+    }
+
+
+    if (lengthArrOneCounter == lengthArrTwoCounter){
+
+        cout << "Both arrays have the same length. Continuing for elemet-wise check ...\n";
+
+    }
+
+    else{
+        cout << "Arrays are not equal. Exiting ...\n";
+    }
+
+
+    // vector<int> vec1 = {1, 2, 3, 4, 5};
+    //vector<int> vec2 = {5, 6, 7, 8, 9};
 
 
     return 0;
