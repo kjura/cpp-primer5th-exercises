77bb7cf892dae988de96565158c1cb9a2dc0af0b
kjura <bulsit565@gmail.com>
2022-09-02 19:36:28 +0200

    exercise: finish 3.38

--- chapter3/exercise038/exercise38.cpp
+++ chapter3/exercise038/exercise38.cpp
@@ -16,7 +16,14 @@ int main()
 
     /*
 
-    
+    Subtraction makes sense, we are calculating the distance between the pointers,
+    thus we now that by moving one pointer or the other by the result of subtraction,
+    we will land in a place in memory that certainly exists and is pointing to something
+    (assuming that both pointers are valid)
+
+    Adding two pointers makes no sense, we could end up in an unexpected region in memory
+    that does not point to anything, it's a sign of possible disaster, we could try to dereference
+    such pointer that is not valid and introduce Undefined behaviour in our program.
 
     */
 
