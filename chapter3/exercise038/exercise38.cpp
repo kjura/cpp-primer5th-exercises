777e4b06c90f63363a8cac70af256cb9e2ab90c4
kjura <bulsit565@gmail.com>
2022-09-02 19:28:11 +0200

    chore: initalize exercise 3.38

---
+++ chapter3/exercise038/exercise38.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+ 
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+    // Why would adding two pointers be meaningless?
+
+
+    /*
+
+    
+
+    */
+
+
+    return 0;
+}
