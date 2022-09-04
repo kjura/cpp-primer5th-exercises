1007e2533822ed4545854525e32f7115b7ad61dd
kjura <bulsit565@gmail.com>
2022-09-04 23:11:29 +0200

    chore: initaize 3.40

---
+++ chapter3/exercise040/exercise40.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+ 
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+    
+
+
+    return 0;
+}
