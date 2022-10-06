01eaee7104bc97c4a739f95c7b00cde7b6bf1cd9
kjura <30084359+kjura@users.noreply.github.com>
2022-10-06 22:00:24 +0200

    finish: finish exercise 3.42

--- chapter3/exercise042/exercise42.cpp
+++ chapter3/exercise042/exercise42.cpp
@@ -7,11 +7,23 @@
 // <br />
 using std::cout;
 using std::endl;
+using std::vector;
 
 int main()
 {
 
+    vector<int> myVec{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int arry[10] = {};
 
+    for (decltype(myVec.size()) i=0; i != myVec.size(); ++i){
+
+        arry[i] = myVec[i];
+
+    }
+
+    for (auto e : arry){
+        cout << e << "\n";
+    }
 
 
     return 0;
