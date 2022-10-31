c836a4f6493219b56bfef02ca171d4c30a435cc3
kjura <30084359+kjura@users.noreply.github.com>
2022-10-31 13:31:26 +0100

    exercise(3.43): add range for with explicit types

--- chapter3/exercise043/exercise43.cpp
+++ chapter3/exercise043/exercise43.cpp
@@ -11,7 +11,25 @@ using std::endl;
 int main()
 {
 
+    int ia[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
 
+
+
+    /* USING RANGE FOR*/
+    for ( int (&row)[4] : ia){
+        for ( int &col : row){
+            cout << col << " ";
+        }
+        cout << "\n";
+    } 
+
+    /*USING ORDINARY FOR LOOP WITH SUBSCRIPTS*/
+
+
+
+
+
+    /*USING ORDINARY FOR LOOP WITH SUBSCRIPTS*/
 
 
     return 0;
