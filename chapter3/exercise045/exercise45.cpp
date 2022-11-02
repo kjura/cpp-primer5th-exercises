c27ba9aa8b7f32fcf95438b2228cfb7f71082618
kjura <30084359+kjura@users.noreply.github.com>
2022-11-02 20:44:17 +0100

    finish: exercise 3.45

--- chapter3/exercise045/exercise45.cpp
+++ chapter3/exercise045/exercise45.cpp
@@ -11,8 +11,38 @@ using std::endl;
 int main()
 {
 
-    
+    int ia[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
 
+    /* USING RANGE FOR*/
+    for ( auto &row : ia){
+        for ( auto &col : row){
+            cout << col << " ";
+        }
+        cout << "\n";
+    } 
+    cout << "\n";
+
+
+    /*USING ORDINARY FOR LOOP WITH SUBSCRIPTS*/
+
+    for (auto row = 0; row != 3; ++row){
+        for(auto col = 0; col != 4; ++col){
+            cout << ia[row][col] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+
+    /*USING ORDINARY FOR LOOP WITH POINTERS*/
+
+
+    for (auto startRow = std::begin(ia); startRow != std::end(ia); ++startRow){
+        for(auto startCol = *startRow; startCol != std::end(*startRow); ++startCol){
+            cout << *startCol << " ";
+        }
+        cout << "\n";
+    }
+    
 
     return 0;
 }
