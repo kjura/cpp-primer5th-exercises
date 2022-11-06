289cb3b24c9426b37b0fbb8f25d1c38460ebafee
kjura <30084359+kjura@users.noreply.github.com>
2022-11-06 22:30:32 +0100

    finish(4.3)

--- chapter4/exercise003/exercise3.cpp
+++ chapter4/exercise003/exercise3.cpp
@@ -11,8 +11,20 @@ using std::endl;
 int main()
 {
 
+    /*
+    
+    No right or wrong answer. If we accept the the status of an undefined order of evaluation we definitely benefit
+    from the fact that our programs can get speeded up. If we are talking about high-performance computing that is 
+    an acceptable trade-off. On the other hand, if we want to program critical software (say, a control mechanism for
+    a spaceship in space) this can unexpectedly blow up to our faces (it can literaly cost other people's lifes) so 
+    it would be nice if the compiler could prevent such things from happening.
 
 
+    Personally, I accept the trade-off, but with the example above, I would not opt for C++ if I had to create something
+    safe and secure.
+    
+    
+    */
 
     return 0;
 }
