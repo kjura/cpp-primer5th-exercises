8106f538a3103cce2e6a18a811b4438ccd036c68
kjura <30084359+kjura@users.noreply.github.com>
2022-11-06 23:44:58 +0100

    finish(4.5)

--- chapter4/exercise005/exercise5.cpp
+++ chapter4/exercise005/exercise5.cpp
@@ -11,7 +11,18 @@ using std::endl;
 int main()
 {
 
+    auto expr1 = -30 * 3 + 21 / 5; // --> -86
 
+    auto expr2 = -30 + 3 * 21 / 5; // --> -18
+
+    auto expr3 = 30 / 3 * 21 % 5; // --> 0
+
+    auto expr4 = -30 / 3 * 21 % 4; // --> -2
+
+    cout << expr1 << endl;
+    cout << expr2 << endl;
+    cout << expr3 << endl;
+    cout << expr4 << endl;
 
 
     return 0;
