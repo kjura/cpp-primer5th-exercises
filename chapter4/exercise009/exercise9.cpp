d55df44d387f3115f185ec331f0544afa5de7096
kjura <30084359+kjura@users.noreply.github.com>
2022-11-08 18:05:35 +0100

    finish(4.9)

--- chapter4/exercise009/exercise9.cpp
+++ chapter4/exercise009/exercise9.cpp
@@ -11,8 +11,21 @@ using std::endl;
 int main()
 {
 
+    const char *cp = "Hello World"; // Pointer to the first element of an array, it points to the address of H
+
+     if (cp && *cp){
+        cout << "True" << endl;
+    }
+    else{
+        cout << "false" << endl;
+    }
     
 
+    // True if cp is not a null pointer and if the character that cp points to is not zero
+    // cp is not a null pointer and it points to letter H, it gets promoted to int (ASCII 72 on my machine)
+    // it is not zero, both conditions are true
+
+    // int my = int('H'); --> ASCII 072
 
     return 0;
 }
