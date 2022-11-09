8559a930e8564bbe0cd8c0e76b53995ff7e90d05
kjura <30084359+kjura@users.noreply.github.com>
2022-11-09 18:09:52 +0100

    finish(4.13)

--- chapter4/exercise013/exercise13.cpp
+++ chapter4/exercise013/exercise13.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <iomanip>
  
 // g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
 // g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
@@ -11,8 +12,25 @@ using std::endl;
 int main()
 {
 
+    int i; double d;
+    cout << std::fixed << std::setprecision(1);
 
+    d = i = 3.5;
+    /*
+    i gets 3.5 assinged, since it's an integer, the truncation happens and i is an int == 3
+    Then the result is assinged to d, but d is a double, so 3 is converted to 3.0 and d == 3.0
+    */
 
+    cout << "d=" << d << ", i=" << i << "\n";
+
+    
+    i = d = 3.5;
+
+    /*
+    d is 3.5, i is an int, truncation takes place and finally i == 3;
+    */
+
+    cout << "d=" << d << ", i=" << i << "\n";
 
     return 0;
 }
