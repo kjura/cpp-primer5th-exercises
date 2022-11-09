2b7b86a8131d7426b30d1cf2cd209c972508f920
kjura <30084359+kjura@users.noreply.github.com>
2022-11-09 19:37:45 +0100

    finish(4.17)

---
+++ chapter4/exercise017/exercise17.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+ 
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+    /*
+    
+    Prefix incrementation -> increments its operand and yields the CHANGED object as its result
+
+    Postfix -> increments the operand but yield a copy of the original, UNCHANGED value as its result
+    
+    */
+
+
+
+
+
+    // int i = 69, j;
+
+    // //j = ++i;
+
+    // j = ++i;
+
+    // cout << "j = " << j << "\n" << "i = " << i << "\n";
+
+
+    return 0;
+}
