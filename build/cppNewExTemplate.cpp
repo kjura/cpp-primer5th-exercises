237267933a7d88251c7b15c0d9fd425521c622dc
kjura <30084359+kjura@users.noreply.github.com>
2022-12-09 18:54:41 +0100

    finish(4.20): add using to a template

--- build/cppNewExTemplate.cpp
+++ build/cppNewExTemplate.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
- 
+#include <string>
+
 // g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
 // g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
 // <br />
 using std::cout;
 using std::endl;
+using std::vector;
+using std::string;
 
 int main()
 {
