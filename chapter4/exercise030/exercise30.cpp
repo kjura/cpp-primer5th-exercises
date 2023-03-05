ef136c991c2caa5e706df957d3cca7d5e8c33ae6
kjura <30084359+kjura@users.noreply.github.com>
2023-03-05 15:38:18 +0100

    finish 4.30

---
+++ chapter4/exercise030/exercise30.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+
+int main()
+{
+
+    /*
+    sizeof x + y == ( (sizeof x) + y ) 
+    sizeof p->mem[i] == sizeof ( (p->mem[i]) )
+    sizeof a < b == ( (size of a) < b ) 
+    sizeof f() == (sizeof ( f() ) )
+    */
+
+    return 0;
+}
