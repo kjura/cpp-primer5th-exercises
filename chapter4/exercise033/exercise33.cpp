3d86b5039de4ab83c2de1d504c74d90a7a0ec48e
kjura <30084359+kjura@users.noreply.github.com>
2023-03-06 22:34:24 +0100

    finish 4.33

--- chapter4/exercise033/exercise33.cpp
+++ chapter4/exercise033/exercise33.cpp
@@ -13,8 +13,23 @@ using std::string;
 
 int main()
 {
+    bool someValue = true;
+    int x = 4;
+    int y = 2;
 
+    /*
+    If someValue is true then (because conditional operator has higher precendance than comma operator)
+    x will be incremented, y is incremented and conditional operator has evaluated itself, what is important
+    is that then comma operator kicks in, so y will be pre-decremented and the final outcome will be
+    x = 5 and y = 2 (--y will always be executed regardless of the outcome)
 
+    If someValue is false then first --x gives x = 3 and comma operator evalues right-hand expression --y giving
+    y = 1
+    */
+
+    someValue ? ++x, ++y : --x, --y;
+
+    cout << "x=" << x << " y=" << y << "\n";
 
 
     return 0;
