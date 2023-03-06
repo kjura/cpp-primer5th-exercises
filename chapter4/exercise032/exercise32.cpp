89e73ecd3e841760948a185bf05348af1042c038
kjura <30084359+kjura@users.noreply.github.com>
2023-03-06 22:21:57 +0100

    finish 4.32

--- chapter4/exercise032/exercise32.cpp
+++ chapter4/exercise032/exercise32.cpp
@@ -14,7 +14,18 @@ using std::string;
 int main()
 {
 
+    constexpr int size = 5;
+    int ia[size] = {1,2,3,4,5};
+    
+    /*
+    The following loop goes (5 times) from the pointer that points to the first element of array ia
+    until it reaches a pointer before off-the-end pointer. It also goes from ix=0 to ix=4 (5 times)
 
+    Every iteration it increments ix and ptr by 1
+    */
+    for (int *ptr = ia, ix = 0; ix != size && ptr != ia+size; ++ix, ++ptr){
+         /* ... SOMETHING   */ 
+         }
 
 
     return 0;
