981f67f9275c9ef7cbc7ac7500715b34c14d4e20
kjura <30084359+kjura@users.noreply.github.com>
2023-06-29 21:25:50 +0200

    structure(chapter5): add file exercise and markdown

---
+++ chapter5/exercise005/README.md
@@ -0,0 +1,2 @@
+Exercise 5.5: Using an if–else statement, write your own version of the
+program to generate the letter grade from a numeric grade.
---
+++ chapter5/exercise005/exercise5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+
+int main()
+{
+
+    return 0;
+}
