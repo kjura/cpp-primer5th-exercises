60c4d440fff086ff56d28bd2195e71bc90785d6a
kjura <30084359+kjura@users.noreply.github.com>
2023-10-10 22:41:46 +0200

    finish: exercise 6.13

---
+++ chapter6/exercise013/exercise13.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+// PLACEHOLDEREXERCISENAME < input.txt > output.txt
+// PLACEHOLDEREXERCISENAME ./exercise5 < input.txt >> output.txt
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cin;
+
+int main()
+{
+
+    /* 
+
+    void f(T) --> This is pass by value --> nothing the function does to the parameter
+    can affect the argument, we operate on a seperate copy of T
+
+    void f(T&) --> This is pass by reference --> a reference parameter is bound
+    directly to the object from which it is initalized, we operate on the original
+    object through the reference (which is just an alias for T)
+
+     */
+
+
+    return 0;
+}
