dd69fbc970abff616d3120f32b072ebc9ef5525d
kjura <30084359+kjura@users.noreply.github.com>
2023-10-10 20:30:05 +0200

    finish: exercise 6.10

--- chapter6/exercise010/exercise10.cpp
+++ chapter6/exercise010/exercise10.cpp
@@ -14,45 +14,40 @@ using std::vector;
 using std::string;
 using std::cin;
 
-void reset(int* ip){
-    *ip = 0;
-    ip = 0;
-}
-
 void swap(int* p1, int* p2){
-    int* address_pt1 = p1;
-    int* address_pt2 = p2;
-
-    *p1 = *address_pt2;
-    *p2 = *address_pt1;
-
+    int temp = *p1; // holds value of p1
+    *p1 = *p2; // change value of p1 to value of p2, p1 is a new value, temp still holds the old p1
+    *p2 = temp; // change value of p2 to the old value of p1 through value in temp
 
 }
 
 int main()
 {
 
+    // The key is to rembemer to use a third variable, it serves as a helper to hold the old state of one of
+    // the variables
+
     int n {69}, i {42};
     int* p {&n};
     int* q {&i};
 
-    cout << "Values before swapping\n";
+    cout << "\nValues before swapping\n";
     cout << "n = " << n << "\n";
     cout << "i = " << i << "\n";
-    cout << "p = " << p << "\n";
-    cout << "q = " << q << "\n";
-    cout << "*p = " << *p << "\n";
-    cout << "*q = " << *q << "\n";
+    // cout << "p = " << p << "\n";
+    // cout << "q = " << q << "\n";
+    // cout << "*p = " << *p << "\n";
+    // cout << "*q = " << *q << "\n";
 
     cout << "\n\nValues after swapping\n";
 
     swap(p, q);
     cout << "n = " << n << "\n";
     cout << "i = " << i << "\n";
-    cout << "p = " << p << "\n";
-    cout << "q = " << q << "\n";
-    cout << "*p = " << *p << "\n";
-    cout << "*q = " << *q << "\n";
+    // cout << "p = " << p << "\n";
+    // cout << "q = " << q << "\n";
+    // cout << "*p = " << *p << "\n";
+    // cout << "*q = " << *q << "\n\n";
 
 
     return 0;
