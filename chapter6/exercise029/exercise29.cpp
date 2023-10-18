6b1eefb37f11efa689e8e2a6846aaad0ee599b9e
kjura <30084359+kjura@users.noreply.github.com>
2023-10-18 19:04:23 +0200

    finish: exercise 6.29

---
+++ chapter6/exercise029/exercise29.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+// PLACEHOLDEREXERCISENAME < input.txt > output.txt
+// PLACEHOLDEREXERCISENAME ./exercise5 < input.txt >> output.txt
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cin;
+
+int main()
+{
+
+    // We could use a reference as the loop control variable
+    // Elements in an initalizer_list are always const values
+    // If we have the initalizer_list of strings these can cost us
+    // memory therefore, we can use reference to const to save time/space
+
+
+    return 0;
+}
