8a8fd2f09d6f032b536335af81270fb062119ae3
kjura <30084359+kjura@users.noreply.github.com>
2023-10-21 01:32:51 +0200

    draft: exercise 6.35 clarify whether the code is UB with someone

--- chapter6/exercise035/exercise35.cpp
+++ chapter6/exercise035/exercise35.cpp
@@ -14,10 +14,34 @@ using std::vector;
 using std::string;
 using std::cin;
 
+int factorial (int val){
+    if (val > 1){
+        return factorial(val - 1) * val;
+        // what if factorial( (val--) - 1) * val instead?
+        // Ten val inside factorial is different than the val we use outside of a function call
+        // For example factorial(((val=4)--) - 1) * val <====> factorial(4 - 1) * 3 instead of factorial(4 - 1) * 4
+    }
+    return 1;
+}
+
 int main()
 {
 
+    /* factorial((val--) - 1) * val
+    
+    factorial(4) --> factorial(3) * 3 --> factorial(2) * 2 --> factorial(1) * 1 --> 1
 
+    3 * 2 * 1 * 1 = 6
+
+    The program would contain a bug and the results would not be true for any number
+
+
+    NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!!!!
+
+
+    The program is corrupted since the very start, this is Undefined Behaviour (order of evaluation for multiplication "*")
+
+     */
 
 
     return 0;
