247b695bfac64325de229b8382c8d48105a7bbc6
kjura <30084359+kjura@users.noreply.github.com>
2023-10-22 22:42:06 +0200

    finish: exercise 6.45

---
+++ chapter6/exercise045/exercise45.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+// PLACEHOLDEREXERCISENAME < input.txt > output.txt
+// PLACEHOLDEREXERCISENAME ./exercise5 < input.txt >> output.txt
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cin;
+
+int main()
+{
+
+    /* See: https://www.learncpp.com/cpp-tutorial/inline-functions/
+
+    Quote:
+
+    """However, in modern C++, the inline keyword is no longer used to request that a
+    function be expanded inline."""
+    
+    Quote:
+    """Modern optimizing compilers are typically very good at determining which functions should be 
+    made inline -- better than humans in most cases. As a result, the compiler will
+    likely ignore or devalue any use of inline to request inline expansion for your functions."""
+    
+    */
+
+
+    return 0;
+}
