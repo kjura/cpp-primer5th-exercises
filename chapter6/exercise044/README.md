dc736758ce7faf2dda804d225d482b5be4e25089
kjura <30084359+kjura@users.noreply.github.com>
2023-10-22 22:37:34 +0200

    finish: exercise 6.44

---
+++ chapter6/exercise044/README.md
@@ -0,0 +1,2 @@
+Exercise 6.44: Rewrite the ```isShorter``` function from § 6.2.2 (p. 211) to be
+inline.
---
+++ chapter6/exercise044/exercise44.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "exercise_44.h"
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+// PLACEHOLDEREXERCISENAME < input.txt > output.txt
+// PLACEHOLDEREXERCISENAME ./exercise5 < input.txt >> output.txt
+using std::cout;
+using std::endl;
+using std::string;
+
+
+int main()
+{
+
+    cout << isShorter("John", "DoeDoe") << endl;
+
+
+    return 0;
+}
