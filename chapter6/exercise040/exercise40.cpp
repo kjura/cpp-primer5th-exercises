2667f16a40935e5e7f9eb4303409cb823efc2f2e
kjura <30084359+kjura@users.noreply.github.com>
2023-10-22 22:00:11 +0200

    finish: exercise 6.40

---
+++ chapter6/exercise040/exercise40.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+// PLACEHOLDEREXERCISENAME < input.txt > output.txt
+// PLACEHOLDEREXERCISENAME ./exercise5 < input.txt >> output.txt
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cin;
+
+int main()
+{
+
+    // LEGAL
+    // int ff(int a, int b = 0, int c = 0);
+
+
+    // ILLEGAL --> If a parameter has a default argument, all the paramters that follow it must also have default args
+    // char* init(int ht = 24, int wd, char bckgrnd);
+
+    return 0;
+}
