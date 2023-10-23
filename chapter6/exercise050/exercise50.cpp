ff45a2f8d48aae72668ee730ac654d56cd4d8817
kjura <30084359+kjura@users.noreply.github.com>
2023-10-23 23:14:27 +0200

    finish: exercise 6.50

--- chapter6/exercise050/exercise50.cpp
+++ chapter6/exercise050/exercise50.cpp
@@ -25,13 +25,21 @@ int main()
     //  (a) f(2.56, 42)
 
     // Viable func --> f(int, int) AND f(double, double = 3.14);
+    // This call is ambigous because of paramter types and
+    // necessary conversions (from double to int and from int to double)
 
 
-    // (b) f(42)
+    // (b) f(42) --> f(int);
+    // Viable func --> f(int)
+    // Best mach --> f(int)
 
     // (c) f(42, 0)
+    // Viable functions --> f(int, int) and f(double, double = 3.14)
+    // best match --> f(int, int)
 
     // (d) f(2.56, 3.14)
+    // Viable functions --> f(int, int) and f(double, double = 3.14)
+    // Best match -> f(double, double = 3.14)
 
 
     return 0;
