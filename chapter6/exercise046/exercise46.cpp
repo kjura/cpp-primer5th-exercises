c9974b88b8ffec656523740c191891a6590df60f
kjura <30084359+kjura@users.noreply.github.com>
2023-10-23 13:31:46 +0200

    finish: exercise 6.46

--- chapter6/exercise046/exercise46.cpp
+++ chapter6/exercise046/exercise46.cpp
@@ -1,7 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
-#include <string>
 
 // g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
 // g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
@@ -10,15 +7,38 @@
 // PLACEHOLDEREXERCISENAME ./exercise5 < input.txt >> output.txt
 using std::cout;
 using std::endl;
-using std::vector;
+
+
+// compare the length of two strings
+// Would it be possible to define isShorter as a constexpr?
+
+
 using std::string;
-using std::cin;
+
+constexpr bool isShorter(const string& s1, const string& s2)
+{
+    return s1.size() < s2.size();
+}
 
 int main()
 {
 
 
+    // Would it be possible to define isShorter as a constexpr?
 
+    // Compiling for C++11/14/17 this yields error: .size() is not constexpr
+
+    //  warning: call to non-'constexpr' function 'std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::size_type std:  
+    // 20 |     return s1.size() < s2.size();
+    //   |            ~~~~~~~^~
+
+
+    // But for c++20 this will work (why?)
+
+    
+
+    // bool result = isShorter("John", "DoeDoe");
+    // cout << result << endl;
 
     return 0;
 }
