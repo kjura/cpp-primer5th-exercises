13e9e6ee4e90c57598feebead7849fbd248ea999
kjura <30084359+kjura@users.noreply.github.com>
2023-12-03 22:30:32 +0100

    draft: add Chapter 7 Exercise 1 with a bug

---
+++ chapter7/exercise001/README.md
@@ -0,0 +1,3 @@
+Exercise 7.1: Write a version of the transaction-processing program from §
+1.6 (p. 24) using the ```Sales_data``` class you defined for the exercises in §
+2.6.1 (p. 72).
---
+++ chapter7/exercise001/exercise1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+// PLACEHOLDEREXERCISENAME < input.txt > output.txt
+// PLACEHOLDEREXERCISENAME ./exercise5 < input.txt >> output.txt
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cin;
+
+
+
+struct Sales_data {
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+int main()
+{
+
+    Sales_data total; // variable to hold data for the next transaction
+    double price;
+    // Read the first transaction
+    if (cin >> total.bookNo >> total.units_sold >> price){
+        // Calculate the revenue for the first transaction
+        total.revenue = total.units_sold * price;
+
+        // Read the next transactions
+        Sales_data trans;
+        while (cin >> trans.bookNo >> trans.units_sold >> price){
+            // If we're still processing the same book, update revenue
+            if (trans.bookNo == total.bookNo){
+                total.units_sold += trans.units_sold;
+                total.revenue += trans.revenue;
+            }
+            else {
+                // Print results for the previous book before processing the next one
+                cout << total.bookNo << " " << total.units_sold << " " << total.revenue << " " << (total.revenue / total.units_sold) << "\n";
+                // Reste total revenue and total unit sold to prepare variables to hold data for the next book
+                total.revenue = 0.0;
+                total.units_sold = 0;
+            }
+        }
+    }
+
+
+    return 0;
+}
