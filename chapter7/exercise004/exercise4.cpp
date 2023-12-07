bae46c8f66f619692b751b3806435b342563e337
kjura <30084359+kjura@users.noreply.github.com>
2023-12-07 17:13:47 +0100

    finish: exercise 7.4

---
+++ chapter7/exercise004/exercise4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+// PLACEHOLDEREXERCISENAME < input.txt > output.txt
+// PLACEHOLDEREXERCISENAME ./exercise5 < input.txt >> output.txt
+// Redirection in powershell  Get-Content .\input.txt | .\<YOUREXERCISENAME>>.exe
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cin;
+
+struct Person {
+    string name = "PLACEHOLDER";
+    string address = "PLACEHOLDER";
+};
+
+int main()
+{
+
+
+
+
+    return 0;
+}
