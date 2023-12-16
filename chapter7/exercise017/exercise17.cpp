449bca923545eafe6097a60db141632b03311cce
kjura <30084359+kjura@users.noreply.github.com>
2023-12-16 14:30:29 +0100

    finish: exercise 7.17

---
+++ chapter7/exercise017/exercise17.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+// PLACEHOLDEREXERCISENAME < input.txt > output.txt
+// PLACEHOLDEREXERCISENAME ./exercise5 < input.txt >> output.txt
+// Redirection in powershell  Get-Content .\input.txt | .\<YOUREXERCISENAME>>.exe
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cin;
+
+int main()
+{
+
+    /* 
+    
+    The only difference is the default access level: using class, the first members defined before an access specifier
+    are private, using struct, we intend to keep all of the class members public
+    
+     */
+
+
+    return 0;
+}
