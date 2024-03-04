33f01161655873d8202ff1d49769f4c10ab1a6b9
kjura <30084359+kjura@users.noreply.github.com>
2024-03-04 23:31:15 +0100

    draft: initalize exercise 7.47

---
+++ chapter7/exercise047/README.md
@@ -0,0 +1,3 @@
+Exercise 7.47: Explain whether the ```Sales_data``` constructor that takes a
+```string``` should be ```explicit```. What are the benefits of making the
+constructor ```explicit```? What are the drawbacks?
---
+++ chapter7/exercise047/exercise47.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include "Sales_data.h"
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+// PLACEHOLDEREXERCISENAME < input.txt > output.txt
+// PLACEHOLDEREXERCISENAME ./exercise5 < input.txt >> output.txt
+// Redirection in powershell  Get-Content .\input.txt | .\<YOUREXERCISENAME>>.exe
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cin;
+using std::istream; 
+using std::ostream;
+
+Sales_data::Sales_data(std::istream &is) 
+{
+	// read will read a transaction from is into this object
+	read(is, *this);
+}
+
+double 
+Sales_data::avg_price() const {
+	if (units_sold)
+		return revenue/units_sold;
+	else
+		return 0;
+}
+
+// add the value of the given Sales_data into this object
+Sales_data& 
+Sales_data::combine(const Sales_data &rhs)
+{
+	units_sold += rhs.units_sold; // add the members of rhs into 
+	revenue += rhs.revenue;       // the members of ``this'' object
+	return *this; // return the object on which the function was called
+}
+
+Sales_data 
+add(const Sales_data &lhs, const Sales_data &rhs)
+{
+	Sales_data sum = lhs;  // copy data members from lhs into sum
+	sum.combine(rhs);      // add data members from rhs into sum
+	return sum;
+}
+
+// transactions contain ISBN, number of copies sold, and sales price
+istream&
+read(istream &is, Sales_data &item)
+{
+	double price = 0;
+	is >> item.bookNo >> item.units_sold >> price;
+	item.revenue = price * item.units_sold;
+	return is;
+}
+
+ostream&
+print(ostream &os, const Sales_data &item)
+{
+	os << item.isbn() << " " << item.units_sold << " " 
+	   << item.revenue << " " << item.avg_price();
+	return os;
+}
+
+int main()
+{
+
+    string null_book = "9-999-999999-9";
+    Sales_data item;
+    item.combine(null_book);
+
+
+
+	return 0;
+}
