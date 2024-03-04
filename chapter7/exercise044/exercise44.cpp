e598e8ca87f8448e8a57b38a2ac20aadbc841261
kjura <30084359+kjura@users.noreply.github.com>
2024-03-04 22:51:18 +0100

    finish: exercise 7.44

---
+++ chapter7/exercise044/exercise44.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+// PLACEHOLDEREXERCISENAME < input.txt > output.txt
+// PLACEHOLDEREXERCISENAME ./exercise5 < input.txt >> output.txt
+// Redirection in powershell  Get-Content .\input.txt | .\<YOUREXERCISENAME>>.exe
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cin;
+
+class NoDefault{
+
+    int something {};
+
+    public:
+        NoDefault(int arg) : something(arg) {cout << "NoDefault constructor with int called" << "\n";};
+
+};
+
+
+class C{
+
+    NoDefault c_no_def;
+    public:
+
+        C() : c_no_def(42) {cout << "C constructor called" << "\n";};
+
+};
+
+int main()
+{
+
+    NoDefault hello(42);
+    C hello_C;
+
+    vector<NoDefault> vec(10); // Error: No matching function for call to <SOME TEMPLATE HELL>
+    // No default constructor for NoDefault
+
+
+    return 0;
+}
