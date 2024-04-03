7c0aaf637c90ad0e5c832bb05eca20d7c57f2e0f
kjura <30084359+kjura@users.noreply.github.com>
2024-04-03 13:00:21 +0200

    finish: exercise 8.3

---
+++ chapter8/exercise003/README.md
@@ -0,0 +1,5 @@
+Exercise 8.3: What causes the following ```while``` to terminate?
+
+```cpp
+while (cin >> i) /* ... */
+```
---
+++ chapter8/exercise003/exercise3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+// PLACEHOLDEREXERCISENAME < input.txt > output.txt
+// PLACEHOLDEREXERCISENAME ./exercise5 < input.txt >> output.txt
+// Redirection in powershell  Get-Content .\input.txt | .\<YOUREXERCISENAME>>.exe
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cin;
+
+int main()
+{
+
+    /* 
+    
+    while (cin >> input) will fail if a call to !fail() yields false 
+
+    so effectively: if either failbit or badbit in the stream is set (or end of file reached)
+    
+    */
+
+
+    return 0;
+}
