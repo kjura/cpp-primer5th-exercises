fb70466ba8656f00aa72163425a6e103123b72f2
kjura <30084359+kjura@users.noreply.github.com>
2024-04-04 10:07:57 +0200

    draft: init exercise 8.4

---
+++ chapter8/exercise004/README.md
@@ -0,0 +1,2 @@
+Exercise 8.4: Write a function to open a file for input and read its contents into a
+`vector` of `strings`, storing each line as a separate element in the `vector`.
---
+++ chapter8/exercise004/exercise4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <fstream>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+// PLACEHOLDEREXERCISENAME < input.txt > output.txt
+// PLACEHOLDEREXERCISENAME ./exercise5 < input.txt >> output.txt
+// Redirection in powershell  Get-Content .\input.txt | .\<YOUREXERCISENAME>>.exe
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cin;
+
+int main()
+{
+
+    vector<string> container {};
+    
+
+
+    return 0;
+}
