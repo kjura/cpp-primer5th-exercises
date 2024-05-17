4557526259e511d29b519954e9f54cd000e3c6ea
kjura <30084359+kjura@users.noreply.github.com>
2024-05-17 17:06:26 +0200

    finish: exercise 9.3

---
+++ chapter9/exercise003/exercise3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+// PLACEHOLDEREXERCISENAME < input.txt > output.txt
+// PLACEHOLDEREXERCISENAME ./exercise5 < input.txt >> output.txt
+// Redirection in powershell  Get-Content .\input.txt | .\<YOUREXERCISENAME>>.exe
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cin;
+
+int main()
+{
+
+    /* 
+    
+    What are the constraints on the iterators that form iterator ranges?
+
+    Two iteratos, begin and end, form an iterator range if (both conditions met):
+
+    1) They refer to elements of, or one past the end of, the same container
+    2) End must not precede begin, it's possible to increment begin and reach end
+    
+    */
+
+
+    return 0;
+}
