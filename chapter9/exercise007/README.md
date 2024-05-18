4c93e5cabe179883404b7f05b1244fa825a2d3b6
kjura <30084359+kjura@users.noreply.github.com>
2024-05-19 00:56:25 +0200

    finish: exercise 9.7

---
+++ chapter9/exercise007/README.md
@@ -0,0 +1,2 @@
+Exercise 9.7: What type should be used as the index into a `vector` of
+`ints`?
---
+++ chapter9/exercise007/exercise7.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+// PLACEHOLDEREXERCISENAME < input.txt > output.txt
+// PLACEHOLDEREXERCISENAME ./exercise5 < input.txt >> output.txt
+// Redirection in powershell  Get-Content .\input.txt | .\<YOUREXERCISENAME>>.exe
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cin;
+
+int main()
+{
+
+
+    /* 
+    
+     What type should be used as the index into a `vector` of
+    `ints`?
+
+
+    std::vector<int>::size_type
+    
+     */
+
+
+
+    return 0;
+}
