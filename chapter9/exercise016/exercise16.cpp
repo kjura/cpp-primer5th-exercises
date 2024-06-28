0c5981dba7053e840c35532a36f6e8bf06d4eaa6
kjura <30084359+kjura@users.noreply.github.com>
2024-06-28 12:23:04 +0200

    draft: add working logic for 9.16 with a lot of redundant code

--- chapter9/exercise016/exercise16.cpp
+++ chapter9/exercise016/exercise16.cpp
@@ -18,18 +18,40 @@ using std::string;
 using std::cin;
 using std::list;
 
+
+
+/* 
+
+Exercise 9.16: Repeat the previous program, but compare elements in a list<int>
+to a vector<int>.
+
+ */
+
+
+/* 
+
+{1, 9, 8, 4} and {1, 9, 8}
+
+{1, 9, 8, 4} and {1, 9, 8, 4}
+
+{1, 9, 8, 5, 7} and {1, 9, 3, 4, 6, 4, 3}
+
+
+ */
+
 int main()
 {
 
-    list<int> l { 1, 9, 8, 4 };
-    vector<int> v { 1, 9, 8, 4};
+    list<int> l { 1, 1, 1, 1, 50, 60};
+    vector<int> v { 1, 1, 42, 1, 1, 1, 7};
 
-    if (l.size() == v.size()) {
+    auto size_of_l { l.size() };
+    auto size_of_v { v.size() };
 
-        cout << "Same size of l and v \n";
+    auto l_beg { l.cbegin() };
+    auto v_beg { v.cbegin() };
 
-        auto l_beg { l.cbegin() };
-        auto v_beg { v.cbegin() };
+    if (size_of_l == size_of_v) {
 
         for (; l_beg != l.cend() && v_beg != v.cend(); ++l_beg, ++v_beg){
             if ( *l_beg != *v_beg ){
@@ -39,13 +61,51 @@ int main()
             
         }
 
-        cout << "Same size of l and v and all the elements are qual so l == v\n";
+        cout << "l == v\n";
+        return 0;
     }
 
     else {
-        cout << "TODO" << endl;
-    }
 
+        if (size_of_l > size_of_v) {
+            
+            for (; l_beg != l.cend() && v_beg != v.cend(); ++l_beg, ++v_beg){
+                if ( *l_beg > *v_beg ){
+                    cout << "l > v\n";
+                    return 0;
+                }
+                else if ( *l_beg < *v_beg ) {
+                    cout << "l < v\n";
+                    return 0;
+                }
+    
+            }
+
+            cout << "l > v\n";
+            return 0;
+
+        }
+        else {
+
+            for (; l_beg != l.cend() && v_beg != v.cend(); ++l_beg, ++v_beg){
+                if ( *l_beg > *v_beg ){
+                    cout << "l > v\n";
+                    return 0;
+                }
+                else if ( *l_beg < *v_beg ) {
+                    cout << "l < v\n";
+                    return 0;
+                }
+    
+            }
+
+            cout << "v > l\n";
+            return 0;
+            
+        }
+
+
+    }
 
     return 0;
 }
