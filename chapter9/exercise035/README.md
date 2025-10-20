2680a4cafb69d050b223d22f2adca2eec2e7fbb9
kjura <30084359+kjura@users.noreply.github.com>
2025-10-20 22:05:54 +0200

    finish: exercise 9.35

---
+++ chapter9/exercise035/README.md
@@ -0,0 +1,2 @@
+Exercise 9.35: Explain the difference between a `vector`’s `capacity` and
+its `size`.
---
+++ chapter9/exercise035/exercise35.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+// PLACEHOLDEREXERCISENAME < input.txt > output.txt
+// PLACEHOLDEREXERCISENAME ./exercise5 < input.txt >> output.txt
+// Redirection in powershell  Get-Content .\input.txt | .\<YOUREXERCISENAME>>.exe
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cin;
+
+int main()
+{
+
+
+    /* 
+    
+    Capacity vs Size:
+
+    Quoting page 357 section 9.4 capacity and size:
+
+    The size of a container is the number of elements it already holds
+
+    The capacity of a container is the number of elements it can hold before more space must be allocated
+    
+    */
+    
+    return 0;
+}
