564d055111f3302d619e8e5d22688df0f4919388
kjura <30084359+kjura@users.noreply.github.com>
2026-05-16 19:52:53 +0200

    finish: exercise 9.48

---
+++ chapter9/exercise048/README.md
@@ -0,0 +1,1 @@
+Exercise 9.48: Given the definitions of name and numbers on page 365, what `does numbers.find(name)` return?
---
+++ chapter9/exercise048/exercise48.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+// g++ -std=c++20 -pedantic -Wall -Wextra -Werror -Wshadow -Wsign-conversion -g sandbox.cpp  -o sandbox
+// g++ -std=c++20 -pedantic -Wall -Wextra -Wsign-conversion -g sandbox.cpp -o sandbox
+// <br />
+// PLACEHOLDEREXERCISENAME < input.txt > output.txt
+// PLACEHOLDEREXERCISENAME ./exercise5 < input.txt >> output.txt
+// Redirection in powershell  Get-Content .\input.txt | .\<YOUREXERCISENAME>>.exe
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cin;
+
+int main()
+{
+
+
+    string numbers("0123456789");
+    string name("r2d2");
+
+    cout << numbers.find(name) << "\n"; // returns npos
+
+    return 0;
+}
