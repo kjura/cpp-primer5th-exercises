edda6a860f4eb8c691bc9fdb0dc48837bc903872
kjura <30084359+kjura@users.noreply.github.com>
2026-06-24 12:30:51 +0200

    draft: add floating point function exercise 9.50

--- chapter9/exercise050/exercise50.cpp
+++ chapter9/exercise050/exercise50.cpp
@@ -17,7 +17,15 @@ using std::vector;
 using std::string;
 using std::cin;
 
-std::vector<string> GenerateRandomVector(size_t NumberCount,int minimum, int maximum) {
+
+template <typename T>
+void print(const T& c) {
+    for (const auto& e : c) {
+        cout << e << "\n";
+    }
+}
+
+std::vector<string> GenerateRandomVector(size_t NumberCount, int minimum, int maximum) {
     std::random_device rd; 
     std::mt19937 gen(rd()); // these can be global and/or static, depending on how you use random elsewhere
 
@@ -36,14 +44,50 @@ std::vector<string> GenerateRandomVector(size_t NumberCount,int minimum, int max
     return output;
 }
 
+
+std::vector<string> GenerateRandomVectorFloatingPoint(size_t NumberCount, double minimum, double maximum) {
+    std::random_device rd;
+    std::mt19937 gen(rd()); // these can be global and/or static, depending on how you use random elsewhere
+
+    std::vector<double> values(NumberCount);
+    std::uniform_real_distribution<> dis(minimum, maximum);
+    std::generate(values.begin(), values.end(), [&]() { return dis(gen); });
+
+    std::vector<string> output {};
+
+    for (auto e : values) {
+        output.push_back(std::to_string(e));
+    }
+
+    return output;
+
+}
+
 int main()
 {
 
-    auto container { GenerateRandomVector(10, 0, 20) };
+    vector<string> container { GenerateRandomVector(4, 0, 10) };
 
+    print(container);
+
+    int sum { 0 };
     for (const auto& e : container) {
-        cout << e << "\n";
+        sum += std::stoi(e);
     }
+
+    cout << "Sum of numbers in container is " << sum << "\n";
+    cout << "\n";
+
+    vector<string> container_floats { GenerateRandomVectorFloatingPoint(4, 0.0, 2.0) };
+    print(container_floats);
+
+    double sum_floats {0};
+    for (const auto& e : container_floats) {
+        sum_floats += std::stod(e);
+    }
+
+    cout << "Sum of floating numbers in container is " << sum_floats << "\n";
+
 
     return 0;
 }
